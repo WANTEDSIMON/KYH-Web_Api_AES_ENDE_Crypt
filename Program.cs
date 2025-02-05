a54380b29e9acb2bf1bf43640dce5decbbc149ff
Simon <Simon>
2025-02-05 17:24:36 +0100

    Added new endpoint to generate random key + ReadMe History

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello Earth 🌎!");
 
+// Generate a new AES key on every request (refresh to get a new one)
+app.MapGet("/key", () => GenerateRandomKey());
+
 app.Run();
+
+string GenerateRandomKey()
+{
+    byte[] keyBytes = new byte[32]; // AES-256 key size
+    RandomNumberGenerator.Fill(keyBytes);
+    return Convert.ToBase64String(keyBytes);
+}
